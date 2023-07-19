0ff880116bc56b960d929be03f627d8861f67a70
aaronmitchell96 <mitchell.aaron96@outlook.com>
2023-07-19 18:03:34 -0400

    home page

--- onerepahead/src/App.js
+++ onerepahead/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,10 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-=======
-      <h1>ONE REP AHEAD</h1>
-      <h2>PT APP</h2>
->>>>>>> Stashed changes
     </div>
   );
 }
